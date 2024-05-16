433b4b3e7accdb5ac3b14dd7f0b662c16fd138d9
Thomas Axmann <thoaxm@gmail.com>
2024-05-16 12:48:04 +0200

    Add Repeat key/Mousing, Remove Tap Dance/Sendstring
    Number/Nav Layer becomes a bit full now:
    
    Right Hand:
    
            x 7 8 9 :
            [ 4 5 6 0
          P ] 1 2 3 .
          N S ( , ) e
      P: print
      N: toggles this Layer
      S: space
    
      x and e are included, because thes enables one to write 4e-4 and 2*x
    
    Left Hand:
    
      HOME PGUP UP   PGDW END
      BKSP LE   DW   RGT  DEL
      ALT  GUI  SFT  CTL  $    %
           *    /    -    +    X
    
      X: to third layer
    
      $ is included beause you now can write $1/$5, not really beause of currency
    
    mousing is added back into the third layer, because I'm now using a trackpad,
    and using the mouse keys of the trackpad is awkward. For easy acces, a toggle
    key is added to the base layer.
    
    Having Callum mods (Modifiers on the home row in an upper layer, or in my case
    below the home row) makes it awkward to retype key combos like <c-+> or <a-,>.
    or something like <g-c-up>. A repeat key, together with an anti-repeat key
    should make it easier to type this. I'll also try to incorporate it in my typing
    of words with double letters.

--- keymap.c
+++ keymap.c
@@ -1,110 +1,85 @@
-#include "sendstring_german.h"
 #include QMK_KEYBOARD_H
-//https://github.com/qmk/qmk_firmware/pull/16174
-#include "flow.h"
-#define TWO_CHAR_FN(fname, char1, char2)                                       \
-  void fname(tap_dance_state_t *state, void *user_data) {                      \
-    if (state->count == 1)                                                     \
-      SEND_STRING(char1);                                                      \
-    else {                                                                     \
-      SEND_STRING(char2);                                                      \
-      reset_tap_dance(state);                                                  \
-    }                                                                          \
-  }
-
-enum { TD_DLRPC };
-TWO_CHAR_FN(dollarpercent, "$", "%")
-
-// clang-format off
-tap_dance_action_t tap_dance_actions[] = {
-    [TD_DLRPC]    = ACTION_TAP_DANCE_FN(dollarpercent),
-};
-// clang-format on
+#include "keymap_german.h"
 
 // flow from daliusd
+// https://github.com/qmk/qmk_firmware/pull/16174
+#include "flow.h"
 const uint16_t flow_config[FLOW_COUNT][2] = {
-    {MO(0), KC_LSFT},
     {MO(2), KC_LALT},
     {MO(2), KC_LGUI},
     {MO(2), KC_LCTL},
     {MO(2), KC_LSFT},
 };
 
-enum custom_keycodes {
-    K_NAV=SAFE_RANGE,
-    K_SYM
-};
+enum custom_keycodes { K_NAV = SAFE_RANGE, K_SYM };
 const uint16_t flow_layers_config[FLOW_LAYERS_COUNT][2] = {
     {K_SYM, 1},
     {K_NAV, 2},
 };
 
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-        // Row 1
-        KC_J, KC_D, KC_U, KC_A, KC_X, /* mid */
-        KC_P, KC_H, KC_L, KC_M, KC_W,
-        // Row 2
-        KC_C, KC_T, KC_I, KC_E, KC_O, /* mid */
-        KC_B, KC_N, KC_R, KC_S, KC_G,
-        // Row 3
-        KC_F, KC_V, DE_UDIA, DE_ADIA, DE_ODIA, KC_ESC, /* mid */
-        KC_ENT, DE_Y, DE_Z, KC_COMMA, KC_DOT, KC_K,
-        // Row 4
-        KC_LEFT, KC_UP, DE_SS, KC_LGUI, KC_LSFT, K_SYM, /* mid */
-	K_NAV, KC_SPC, KC_TAB, KC_Q, KC_DOWN, KC_RIGHT),
-    [1] = LAYOUT(
-        // Row 1
-        DE_AT, DE_UNDS, DE_LBRC, DE_RBRC, DE_CIRC, /* mid */
-        DE_EXLM, DE_LABK, DE_RABK, DE_EQL, DE_AMPR,
-        // Row 2
-        DE_BSLS, DE_SLSH, DE_LCBR, DE_RCBR, DE_ASTR, /* mid */
-        DE_QUES, DE_LPRN, DE_RPRN, DE_MINS, DE_COLN,
-        // Row 3
-        DE_HASH, DE_DLR, DE_PIPE, DE_TILD, DE_GRV, KC_NO, /* mid */
-        KC_NO, DE_PLUS, DE_PERC, DE_DQUO, DE_QUOT, DE_SCLN,
-        // Row 4
-        DE_SECT, KC_NO, RSA(KC_S), KC_TRNS, KC_NO, KC_TRNS,  /* mid */
-        MO(3), DE_EURO, KC_NO, KC_NO, KC_NO, KC_NO),
-    [2] = LAYOUT(
-        // Row 1
-        KC_HOME, KC_PGUP, KC_UP, KC_PGDN, KC_END, KC_NO, KC_7, KC_8, KC_9,
-        DE_COLN,
-        // Row 2
-        KC_BACKSPACE, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL, DE_LBRC, KC_4, KC_5,
-        KC_6, KC_0,
-        // Row 3
-        OSM(MOD_LALT), OSM(MOD_LGUI), OSM(MOD_LSFT), OSM(MOD_LCTL), KC_NO,
-        KC_NO, KC_NO, DE_RBRC, KC_1, KC_2, KC_3, KC_DOT,
-        // Row 4
-        KC_PSCR, KC_NO, KC_LALT, KC_LCTL, KC_NO, KC_TRNS, KC_E, DE_MINS,
-        DE_LPRN, TD(TD_DLRPC), DE_RPRN, DE_PLUS),
-    [3] = LAYOUT(
-        // Row 1
-        KC_PWR, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F7, KC_F8, KC_F9, KC_F12,
-        // Row 2
-        KC_VOLU, KC_MUTE, KC_NO, KC_BRIU, KC_NO, KC_NO, KC_F4, KC_F5, KC_F6,
-        KC_F10,
-        // Row 3
-        KC_VOLD, KC_NO, KC_NO, KC_BRID, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1,
-        KC_F2, KC_F3, KC_F11,
-        // Row 4
-        KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO,
-        KC_NO, KC_NO)};
-
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (!update_flow(keycode, record->event.pressed, record->event.key)) return false;
-    return true;
+  if (!update_flow(keycode, record->event.pressed, record->event.key))
+    return false;
+  return true;
 }
 
-void matrix_scan_user(void) {
-    flow_matrix_scan();
-}
+void matrix_scan_user(void) { flow_matrix_scan(); }
 
 #if defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
 
 };
 #endif // defined(ENCODER_ENABLE) && defined(ENCODER_MAP_ENABLE)
+
+// clang-format off
+// My actual layout
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+ [0] = LAYOUT(
+#define _AE_ DE_ADIA
+#define _OE_ DE_ODIA
+#define _UE_ DE_UDIA
+#define _SFT_ OSM(MOD_LSFT)
+//┌────────┬───────┬────────┬────────┬───────┐                            ┌────────┬────────┬─────────┬────────┬────────┐
+    KC_J,    KC_D,   KC_U,    KC_A,    KC_X,                                KC_P,    KC_H,    KC_L,    KC_M,    KC_W,
+//├────────┼───────┼────────┼────────┼───────┤                            ├────────┼────────┼─────────┼────────┼────────┤
+    KC_C,    KC_T,   KC_I,    KC_E,    KC_O,                                KC_B,    KC_N,    KC_R,    KC_S,    KC_G,
+//├────────┼───────┼────────┼────────┼───────┼────────┐          ┌────────┼────────┼────────┼─────────┼────────┼────────┤
+    KC_F,    KC_V,   _UE_,    _AE_,    _OE_,  KC_LGUI,            KC_TAB,   DE_Y,    DE_Z,    KC_COMM, KC_DOT,  KC_K,
+//├────────┼───────┼────────┼────────┼───────┼────────┤          ├────────┼────────┼────────┼─────────┼────────┼────────┤
+    KC_ESC,  TG(3),  DE_SS,   QK_AREP, _SFT_,  K_SYM,             K_NAV,    KC_SPC,  QK_REP,  KC_Q,    KC_NO,   KC_ENT),
+//└────────┴───────┴────────┴────────┴───────┴────────┘          └────────┴────────┴────────┴─────────┴────────┴────────┘
+ [1] = LAYOUT(
+//┌────────┬──────────┬──────────┬─────────┬─────────┐                  ┌────────┬────────┬────────┬────────┬────────┐
+    DE_AT,   DE_UNDS,  DE_LBRC,   DE_RBRC,  DE_CIRC,                     DE_EXLM, DE_LABK, DE_RABK, DE_EQL,  DE_AMPR,
+//├────────┼──────────┼──────────┼─────────┼─────────┤                  ├────────┼────────┼────────┼────────┼────────┤
+    DE_BSLS, DE_SLSH,  DE_LCBR,   DE_RCBR,  DE_ASTR,                     DE_QUES, DE_LPRN, DE_RPRN, DE_MINS, DE_COLN,
+//├────────┼──────────┼──────────┼─────────┼─────────┼──────┐    ┌──────┼────────┼────────┼────────┼────────┼────────┤
+    DE_HASH, DE_DLR,   DE_PIPE,   DE_TILD,  DE_GRV,   KC_NO,      KC_NO, DE_PLUS, DE_PERC, DE_DQUO, DE_QUOT, DE_SCLN,
+//├────────┼──────────┼──────────┼─────────┼─────────┼──────┤    ├──────┼────────┼────────┼────────┼────────┼────────┤
+    DE_SECT, KC_NO,    RSA(KC_S), KC_NO,    KC_NO,    TG(1),      OSL(3), DE_EURO, KC_NO,   KC_NO,   KC_NO,   KC_NO),
+//└────────┴──────────┴──────────┴─────────┴─────────┴──────┘    └──────┴────────┴────────┴────────┴────────┴────────┘
+ [2] = LAYOUT(
+#define _ALT_ OSM(MOD_LALT)
+#define _GUI_ OSM(MOD_LGUI)
+#define _CTL_ OSM(MOD_LCTL)
+//┌────────┬──────────┬─────────┬─────────┬─────────┐                  ┌────────┬────────┬────────┬────────┬────────┐
+    KC_HOME, KC_PGUP,  KC_UP,     KC_PGDN,  KC_END,                      KC_X,    KC_7,    KC_8,    KC_9,    DE_COLN,
+//├────────┼──────────┼─────────┼─────────┼─────────┤                  ├────────┼────────┼────────┼────────┼────────┤
+    KC_BSPC, KC_LEFT,  KC_DOWN,   KC_RGHT,  KC_DEL,                      DE_LBRC, KC_4,    KC_5,    KC_6,    KC_0,
+//├────────┼──────────┼─────────┼─────────┼─────────┼──────┐    ┌──────┼────────┼────────┼────────┼────────┼────────┤
+    _ALT_,   _GUI_,    _SFT_,     _CTL_,    DE_DLR,  DE_PERC,   KC_PSCR, DE_RBRC, KC_1,    KC_2,    KC_3,    KC_DOT,
+//├────────┼──────────┼─────────┼─────────┼─────────┼──────┤    ├──────┼────────┼────────┼────────┼────────┼────────┤
+    KC_NO,   DE_ASTR,  DE_SLSH,  DE_MINS,   DE_PLUS, OSL(3),     TG(2),  KC_SPC,  DE_LPRN, KC_COMM, DE_RPRN, KC_E),
+//└────────┴──────────┴─────────┴─────────┴─────────┴──────┘    └──────┴────────┴────────┴────────┴────────┴────────┘
+  [3] = LAYOUT(
+//┌────────┬──────────┬─────────┬────────┬───────┐                         ┌──────┬───────┬────────┬─────────┬─────────┐
+    KC_PWR,  KC_WH_L,  KC_WH_U,  KC_WH_D, KC_WH_R,                          KC_NO, KC_F7,   KC_F8,  KC_F9,    KC_F12,
+//├────────┼──────────┼─────────┼────────┼───────┤                         ├──────┼───────┼────────┼─────────┼─────────┤
+    KC_VOLU, KC_LGUI,  KC_LSFT,  KC_LCTL, KC_BRIU,                          KC_NO, KC_F4,   KC_F5,  KC_F6,    KC_F10,
+//├────────┼──────────┼─────────┼────────┼───────┼──────┐           ┌──────┼──────┼───────┼────────┼─────────┼─────────┤
+    KC_VOLD, KC_NO,    KC_NO,    KC_NO,   KC_BRID, KC_NO,            KC_NO, KC_NO, KC_F1,   KC_F2,  KC_F3,    KC_F11,
+//├────────┼──────────┼─────────┼────────┼───────┼──────┤           ├──────┼──────┼───────┼────────┼─────────┼─────────┤
+    KC_MUTE, KC_NO,    KC_NO,    KC_BTN2, KC_BTN1, TG(3),            TG(3), KC_NO, KC_MS_L, KC_MS_D,KC_MS_U,  KC_MS_R)
+//└────────┴──────────┴─────────┴────────┴───────┴──────┘           └──────┴──────┴───────┴────────┴─────────┴─────────┘
+};
+// clang-format on
